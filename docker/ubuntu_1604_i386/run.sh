302cb5a97a5521b8f7fb45849f4270f8064243c5
Hiroki Shirokura <slank.dev@gmail.com>
2020-08-18 18:43:05 +0900

    2020.08.18-18:43

--- docker/ubuntu_1604_i386/run.sh
+++ docker/ubuntu_1604_i386/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-IMG=slankdev/frr-dev:ubuntu-18.06-i386
+IMG=slankdev/frr-dev:ubuntu-16.04-i386
 NAM=frr-ubuntu1604-i386
 docker run -td --privileged \
+  --memory=2000mb --memory-swap=2000mb --memory-swappiness=0 \
   -v /root/git/frr:/root/git/frr \
   -v /tmp:/tmp \
-  --name $NAM -hostname $NAM $IMG
+  --name $NAM --hostname $NAM $IMG
