6908a74234741d3fd379ed9a9d11f4b569d76648
Hiroki Shirokura <slank.dev@gmail.com>
2020-08-20 16:23:06 +0900

    2020.08.20-16:23

--- docker/ubuntu_1804_amd64/run.sh
+++ docker/ubuntu_1804_amd64/run.sh
@@ -2,8 +2,8 @@
 IMG=slankdev/frr-dev:ubuntu-18.04-amd64
 NAM=frr-ubuntu1804-amd64
 docker run -td --privileged \
-  --memory=2000mb \
-  --memory-swap=2000mb \
+  --memory=4000mb \
+  --memory-swap=4000mb \
   --memory-swappiness=0 \
   -v /root/git/frr:/root/git/frr \
   -v /tmp:/tmp \
