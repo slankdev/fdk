85b88164f07e7da579565f6411b597ab10202e2f
Hiroki Shirokura <slank.dev@gmail.com>
2020-12-30 01:13:30 +0000

    2020.12.30-01:13

--- bin/fdk-local.bash
+++ bin/fdk-local.bash
@@ -1,7 +1,46 @@
+FDK_FRR_PATH=~/frr
+# FDK_TOPOTEST_TARGET="bgp_prefix_sid2/test_bgp_prefix_sid2.py"
+# FDK_TOPOTEST_TARGET="bgp_srv6l3vpn_to_bgp_vrf/test_bgp_srv6l3vpn_to_bgp_vrf.py"
+FDK_TOPOTEST_TARGET="bgp-vrf-route-leak-basic/test_bgp-vrf-route-leak-basic.py"
+FDK_TOPOTEST_TARGETS="\
+	bgp-vrf-route-leak-basic/test_bgp-vrf-route-leak-basic.py
+	bgp_prefix_sid2/test_bgp_prefix_sid2.py
+	bgp_srv6l3vpn_to_bgp_vrf/test_bgp_srv6l3vpn_to_bgp_vrf.py"
+
+alias cdf="cd $FDK_FRR_PATH"
+alias cdt="cd $FDK_FRR_PATH/tests/topotests/"
+alias vifdk='vim ~/git/fdk/bin/fdk-local.bash && source ~/git/fdk/bin/fdk-local.bash'
+
+function c() {
+	cd $FDK_FRR_PATH
+	make clean
+	./bootstrap.sh
+	./configure \
+    --prefix=/usr \
+    --includedir=\${prefix}/include \
+    --enable-exampledir=\${prefix}/share/doc/frr/examples \
+    --bindir=\${prefix}/bin \
+    --sbindir=\${prefix}/lib/frr \
+    --libdir=\${prefix}/lib/frr \
+    --libexecdir=\${prefix}/lib/frr \
+    --localstatedir=/var/run/frr \
+    --sysconfdir=/etc/frr \
+    --with-moduledir=\${prefix}/lib/frr/modules \
+    --with-libyang-pluginsdir=\${prefix}/lib/frr/libyang_plugins \
+    --enable-configfile-mask=0640 \
+    --enable-logfile-mask=0640 \
+    --enable-snmp=agentx \
+    --enable-multipath=64 \
+    --enable-user=frr \
+    --enable-group=frr \
+    --enable-vty-group=frrvty \
+    --with-pkg-git-version \
+    --with-pkg-extra-version=-MyOwnFRRVersion \
+		#end
+}
 
 function cb() {
-	C=/home/ubuntu/frr.tmp
-	cd $C
+	cd $FDK_FRR_PATH
 	./bootstrap.sh
 	./configure \
     --prefix=/usr \
@@ -24,106 +63,35 @@ function cb() {
     --enable-vty-group=frrvty \
     --with-pkg-git-version \
     --with-pkg-extra-version=-MyOwnFRRVersion \
-		--enable-lttng \
-		# --enable-werror \
-		# --enable-address-sanitizer \
-		# --enable-thread-sanitizer \
-		# --enable-memory-sanitizer \
 		#end
-	make clean && make -C $C -j16 && make -C $C -j16 install
+	make clean && make -C $FDK_FRR_PATH -j16 && make -C $FDK_FRR_PATH -j16 install
 }
 
-function cb-no-rfapi() {
-	C=/home/ubuntu/frr.tmp
-	cd $C
-	./bootstrap.sh
-	./configure \
-    --prefix=/usr \
-    --includedir=\${prefix}/include \
-    --enable-exampledir=\${prefix}/share/doc/frr/examples \
-    --bindir=\${prefix}/bin \
-    --sbindir=\${prefix}/lib/frr \
-    --libdir=\${prefix}/lib/frr \
-    --libexecdir=\${prefix}/lib/frr \
-    --localstatedir=/var/run/frr \
-    --sysconfdir=/etc/frr \
-    --with-moduledir=\${prefix}/lib/frr/modules \
-    --with-libyang-pluginsdir=\${prefix}/lib/frr/libyang_plugins \
-    --enable-configfile-mask=0640 \
-    --enable-logfile-mask=0640 \
-    --enable-snmp=agentx \
-    --enable-multipath=64 \
-    --enable-user=frr \
-    --enable-group=frr \
-    --enable-vty-group=frrvty \
-    --with-pkg-git-version \
-    --with-pkg-extra-version=-MyOwnFRRVersion \
-    --disable-bgp-vnc \
-		--enable-lttng \
-		# --enable-werror \
-		# --enable-address-sanitizer \
-		# --enable-thread-sanitizer \
-		# --enable-memory-sanitizer \
-		#end
-	make clean && make -C $C -j16 && make -C $C -j16 install
+function ta() {
+	for file in $FDK_TOPOTEST_TARGETS; do
+		pytest -s -v --log-level=DEBUG $FDK_FRR_PATH/tests/topotests/$file;
+	done
 }
 
-function b() {
-	C=/home/ubuntu/frr.tmp
-  make -C $C -j16 && make -C $C -j16 install
-}
+function b() { make -C $FDK_FRR_PATH -j16 && make -C $FDK_FRR_PATH -j16 install; }
+function t() { pytest -s -v --log-level=DEBUG $FDK_FRR_PATH/tests/topotests/$FDK_TOPOTEST_TARGET; }
+function tt() { pytest -s -v --log-level=DEBUG --topology-only $FDK_FRR_PATH/tests/topotests/$FDK_TOPOTEST_TARGET; }
+function cbtt() { cb && tt; }
+function bt() { b && t; }
+function btt() { b && tt; }
 
-function t() {
-	C=/home/ubuntu/frr.tmp
-	cd $C/tests/topotests/bgp_srv6l3vpn_to_bgp_vrf
-	test_bgp_srv6l3vpn_to_bgp_vrf.py
-}
+function r1p() { echo $(ps aux | grep mininet:r1 | grep -v grep | awk '{print $2}'); }
+function r2p() { echo $(ps aux | grep mininet:r2 | grep -v grep | awk '{print $2}'); }
 
-function tt() {
-	C=/home/ubuntu/frr.tmp
-	cd $C/tests/topotests/bgp_srv6l3vpn_to_bgp_vrf
-	test_bgp_srv6l3vpn_to_bgp_vrf.py --topology-only
-}
+function r1()  { nsenter -a -t $(ps aux | grep mininet:r1 | grep -v grep | awk '{print $2}') bash --norc; }
+function r2()  { nsenter -a -t $(ps aux | grep mininet:r2 | grep -v grep | awk '{print $2}') bash --norc; }
+function r3()  { nsenter -a -t $(ps aux | grep mininet:r3 | grep -v grep | awk '{print $2}') bash --norc; }
+function r1v() { nsenter -a -t $(ps aux | grep mininet:r1 | grep -v grep | awk '{print $2}') vtysh ; }
+function r2v() { nsenter -a -t $(ps aux | grep mininet:r2 | grep -v grep | awk '{print $2}') vtysh ; }
 
-function cbtt() {
-	cb && tt
-}
-
-function cb-no-rfapi-tt() {
-	cb-no-rfapi && tt
-}
-
-function bt() {
-	b && t
-}
-
-function btt() {
-	b && tt
-}
-
-function r1p() {
-  echo $(ps aux | grep mininet:r1 | grep -v grep | awk '{print $2}')
-}
-
-function r1() {
-	if [ $# -ne 0 ]; then
-		nsenter -a -t $(ps aux | grep mininet:r1 | grep -v grep | awk '{print $2}') $*
-	else
-		nsenter -a -t $(ps aux | grep mininet:r1 | grep -v grep | awk '{print $2}') vtysh
-	fi
-}
-
-function r2p() {
-  echo $(ps aux | grep mininet:r2 | grep -v grep | awk '{print $2}')
-}
-
-function r2() {
-	if [ $# -ne 0 ]; then
-		nsenter -a -t $(ps aux | grep mininet:r2 | grep -v grep | awk '{print $2}') $*
-	else
-		nsenter -a -t $(ps aux | grep mininet:r2 | grep -v grep | awk '{print $2}') vtysh
-	fi
-}
-
-alias cdt='cd ~/frr.tmp/tests/topotests/bgp_srv6l3vpn*'
-alias vifdk='vim ~/git/fdk/bin/fdk-local.bash && source ~/git/fdk/bin/fdk-local.bash'
+function ce1() { nsenter -a -t `ps aux | grep mininet:ce1 | grep -v grep | awk '{print $2}'` bash --norc; }
+function ce2() { nsenter -a -t `ps aux | grep mininet:ce2 | grep -v grep | awk '{print $2}'` bash --norc; }
+function ce3() { nsenter -a -t `ps aux | grep mininet:ce3 | grep -v grep | awk '{print $2}'` bash --norc; }
+function ce4() { nsenter -a -t `ps aux | grep mininet:ce4 | grep -v grep | awk '{print $2}'` bash --norc; }
+function ce5() { nsenter -a -t `ps aux | grep mininet:ce5 | grep -v grep | awk '{print $2}'` bash --norc; }
+function ce6() { nsenter -a -t `ps aux | grep mininet:ce6 | grep -v grep | awk '{print $2}'` bash --norc; }
