ee775ac946d68ef57fbce5aa9a65a7cf92ab5ef3
Hiroki Shirokura <slank.dev@gmail.com>
2021-04-03 09:05:08 +0000

    2021.04.03-09:05

--- bin/fdk-local.bash
+++ bin/fdk-local.bash
@@ -1,7 +1,10 @@
+FDK_FRR_PATH=~/frr.frrouting
 FDK_FRR_PATH=~/frr
-FDK_TOPOTEST_TARGET=bgp_multi_vrf_topo1/test_bgp_multi_vrf_topo1.py
-FDK_TOPOTEST_TARGET=bgp_srv6l3vpn_to_bgp_vrf/test_bgp_srv6l3vpn_to_bgp_vrf.py
 FDK_TOPOTEST_TARGET=bgp_multi_vrf_topo2/test_bgp_multi_vrf_topo2.py
+FDK_TOPOTEST_TARGET=bgp_multi_vrf_topo1/test_bgp_multi_vrf_topo1.py
+FDK_TOPOTEST_TARGET=bgp_l3vpn_to_bgp_vrf/test_bgp_l3vpn_to_bgp_vrf.py
+FDK_TOPOTEST_TARGET=bgp_srv6l3vpn_to_bgp_vrf/test_bgp_srv6l3vpn_to_bgp_vrf.py
+FDK_TOPOTEST_TARGET=srv6_locator/test_srv6_locator.py
 FDK_TOPOTEST_TARGETS="\
 	bgp_multi_vrf_topo1/test_bgp_multi_vrf_topo1.py
 	bgp-vrf-route-leak-basic/test_bgp-vrf-route-leak-basic.py
@@ -9,11 +12,19 @@ FDK_TOPOTEST_TARGETS="\
 	bgp_prefix_sid2/test_bgp_prefix_sid2.py
 	bgp_srv6l3vpn_to_bgp_vrf/test_bgp_srv6l3vpn_to_bgp_vrf.py
 	"
+FDK_TOPOTEST_TARGETS="\
+	zebra_seg6_route/test_zebra_seg6_route.py
+	zebra_seg6local_route/test_zebra_seg6local_route.py
+  srv6_locator/test_srv6_locator.py
+	bgp_prefix_sid2/test_bgp_prefix_sid2.py
+	bgp_srv6l3vpn_to_bgp_vrf/test_bgp_srv6l3vpn_to_bgp_vrf.py
+	"
 
 alias cdf="cd $FDK_FRR_PATH"
 alias cdt="cd $FDK_FRR_PATH/tests/topotests/"
 alias vifdk='vim ~/git/fdk/bin/fdk-local.bash && source ~/git/fdk/bin/fdk-local.bash'
-alias fdkpush='cd ~/git/fdk && git add . && git commit -m update && git push'
+alias fdk-push='cd ~/git/fdk && git add . && git commit -m update && git push'
+alias vifdk-test-target="vim $FDK_FRR_PATH/tests/topotests/$FDK_TOPOTEST_TARGET"
 
 function c() {
 	cd $FDK_FRR_PATH
@@ -40,6 +51,8 @@ function c() {
     --enable-vty-group=frrvty \
     --with-pkg-git-version \
     --with-pkg-extra-version=-MyOwnFRRVersion \
+    --enable-sharpd \
+		--enable-doc-html \
 		#end
 }
 
@@ -67,22 +80,39 @@ function cb() {
     --enable-vty-group=frrvty \
     --with-pkg-git-version \
     --with-pkg-extra-version=-MyOwnFRRVersion \
+    --enable-sharpd \
+		--enable-doc-html \
 		#end
 	make clean && make -C $FDK_FRR_PATH -j16 && make -C $FDK_FRR_PATH -j16 install
 }
 
 function ta() {
+	echo fdk-test-all
+}
+
+function fdk-test-list() {
+	for file in $FDK_TOPOTEST_TARGETS; do
+		echo $file;
+	done
+}
+
+function fdk-test-all() {
 	for file in $FDK_TOPOTEST_TARGETS; do
 		pytest -s -v --log-level=DEBUG $FDK_FRR_PATH/tests/topotests/$file;
 	done
 }
 
+function fdk-test-one() { pytest -s -vv --log-level=DEBUG $FDK_FRR_PATH/tests/topotests/$FDK_TOPOTEST_TARGET; }
+function t() { echo fdk-test-one; }
+
+function fdk-test-one-topology-only() { pytest -s -v --log-level=DEBUG --topology-only $FDK_FRR_PATH/tests/topotests/$FDK_TOPOTEST_TARGET; }
+function tt() { echo fdk-test-one-topology-only; }
+
 function l() { ps aux | grep mininet | grep -v grep | awk '{ print $14 }' | awk -F: '{ print $2 }'; }
 function b() { make -C $FDK_FRR_PATH -j16 && make -C $FDK_FRR_PATH -j16 install; }
-function t() { pytest -s -v --log-level=DEBUG $FDK_FRR_PATH/tests/topotests/$FDK_TOPOTEST_TARGET; }
-function tt() { pytest -s -v --log-level=DEBUG --topology-only $FDK_FRR_PATH/tests/topotests/$FDK_TOPOTEST_TARGET; }
 function cbtt() { cb && tt; }
 function bt() { b && t; }
+function bta() { b && ta; }
 function btt() { b && tt; }
 
 function r1p() { echo $(ps aux | grep mininet:r1 | grep -v grep | awk '{print $2}'); }
@@ -103,7 +133,7 @@ function ce6() { nsenter -a -t `ps aux | grep mininet:ce6 | grep -v grep | awk '
 
 function fdk-list() { l; }
 
-function fdk-tt() {
+function fdk-enter() {
   if [ $# -lt 1 ]; then
     echo "invalid command syntax" 1>&2
     echo "Usage: $0 <mininet-node>" 1>&2
@@ -118,7 +148,7 @@ function fdk-tt() {
 	fi
 }
 
-function _fdk-tt() {
+function _fdk-enter() {
   local args
   case $COMP_CWORD in
   1) args=$(l ${COMP_WORDS[1]}) ;;
@@ -127,4 +157,4 @@ function _fdk-tt() {
   COMPREPLY=( `compgen -W "$args" -- ${COMP_WORDS[COMP_CWORD]}` )
 }
 
-complete -F _fdk-tt fdk-tt
+complete -F _fdk-enter fdk-enter
