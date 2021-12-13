eca1d7443744abd7975ccdd2cc5ce58411cd8505
nyatsume <35296977+Nyatsume@users.noreply.github.com>
2021-12-13 11:08:14 +0900

    Support ubuntu (#5)
    * add support for ubuntu
    
    Co-authored-by: Drumato <drumato43@gmail.com>
    
    * add support for ubuntu
    
    Co-authored-by: Drumato <drumato43@gmail.com>
    
    Co-authored-by: Drumato <drumato43@gmail.com>

---
+++ direct-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xe
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init --path)"' >> ~/.bash_profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+. ~/.bash_profile
+
+pyenv install 3.8.0
+pyenv global 3.8.0
+pip install pipenv
+
+sudo apt -y install libvirt-dev libpython3-dev
+git clone https://github.com/slankdev/fdk.git
+cd fdk
+pipenv sync
+cd playbooks
+pipenv run ansible-playbook direct-ubuntu.yaml -c local -K
