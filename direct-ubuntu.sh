bd08f9e419b0120c5fedbcedb650122c20ce5d4a
Seiki Makino <ma@kino.ma>
2022-01-09 17:42:43 +0900

    Improve reproducibility and portability of `direct-ubuntu.sh` (#7)
    * detect the login shell in `direct-ubuntu.sh`
    
    * use `cat` instead of built-in echo
    
    `echo -e` does not work sometimes
    
    * install packages required by Python 3.8.0
    
    * save exports script temporarily
    
    * install pyenv only if not installed
    
    * fix wrong condition
    
    * always load export script
    
    * skip installation of existing pyhton version

--- direct-ubuntu.sh
+++ direct-ubuntu.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
-set -xe
+set -eux
+
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init --path)"' >> ~/.bash_profile
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-. ~/.bash_profile
+PYENV_EXPORTS="/tmp/pyenv-exports.sh"
+cat << 'EOF' | tee "$PYENV_EXPORTS"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi
+EOF
 
-pyenv install 3.8.0
+# if pyenv is not installed yet, install it
+if [ ! -d $HOME/.pyenv ]; then
+    SHELL_NAME="${SHELL##*/}"
+    if [ "$SHELL_NAME" = "bash" ]; then
+        PROFILE="$HOME/.bash_profile"
+    elif [ "$SHELL_NAME" = "zsh" ]; then
+        PROFILE="$HOME/.zshrc"
+    else
+        echo "Unknown shell. Skipped writing pyenv initializations to profile files."
+        echo "You can manually add the following script to your shell profile:"
+        echo ""
+        cat $PYENV_EXPORTS
+        echo ""
+    fi
+
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    cat $PYENV_EXPORTS >> $PROFILE
+fi
+
+. $PYENV_EXPORTS
+
+sudo apt -y install libvirt-dev libpython3-dev zlib1g-dev libssl-dev
+
+pyenv install --skip-existing 3.8.0
 pyenv global 3.8.0
 pip install pipenv
 
-sudo apt -y install libvirt-dev libpython3-dev
 git clone https://github.com/slankdev/fdk.git
 cd fdk
 pipenv sync
