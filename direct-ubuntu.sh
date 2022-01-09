d9fa77acdce5dc129029d137fde6e091f645bc3b
kino-ma <ma@kino.ma>
2022-01-09 16:20:52 +0900

    use `cat` instead of built-in echo
    `echo -e` does not work sometimes

--- direct-ubuntu.sh
+++ direct-ubuntu.sh
@@ -15,10 +15,16 @@ fi
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init --path)"' >> ~/.bash_profile
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+
+cat >> $PROFILE << 'EOF'
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi'
+EOF
+
 . ~/.bash_profile
 
 pyenv install 3.8.0
